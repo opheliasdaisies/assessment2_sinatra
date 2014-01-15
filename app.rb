5c09500768250a42997537bc35c3c0e9e2cdbea2
Sara Gorecki <sgorecki@gmail.com>
2014-01-15 13:33:31 -0500

    plan routes for app

--- app.rb
+++ app.rb
@@ -6,9 +6,33 @@ set :database, "sqlite3:///spacecats.db" #had to look up syntax for connecting d
 module Space
   class App < Sinatra::Application
 
-    get "/" do
-      "test"
+    get "/index" do
+      #index
     end
+
+    get "/new" do
+      #new
+    end
+
+    post "/index" do
+      #create
+    end
+
+    get "/:id/" do
+      #show
+    end
+
+    # get "/:id/edit" do
+    #   #edit
+    # end
+
+    # put "/:id" do
+    #   #update
+    # end
+
+    # delete "/:id" do
+    #   #destroy
+    # end
 
   end
 end
